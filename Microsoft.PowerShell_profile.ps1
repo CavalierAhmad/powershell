701e53a05c565a0faf81bd2d5cb267d1ac1acd57
ahmad <alja0062@algonquinlive.com>
2023-10-03 18:10:07 -0400

    FleetDatabase Module done & functional although need some polish. Now moving to expenses

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,19 +11,19 @@ function log ($log) {$log >> $logFile}
 
 # Modules
 Import-Module .\Modules\AssignmentManager\AssignmentManager.psm1 -verbose # TO DO           last
-Import-Module .\Modules\ChoreManager\ChoreManager.psm1  -verbose          # TO DO           low
+Import-Module .\Modules\ChoreManager\ChoreManager.psm1           -verbose # TO DO           low
 Import-Module .\Modules\CredentialManager\CredentialManager.psm1 -verbose # TODO           high
-Import-Module .\Modules\Cryptography\Cryptography.psm1 -verbose
-Import-Module .\Modules\EventManager\EventManager.psm1     -verbose       # TODO           high
-Import-Module .\Modules\ExpenseManager\ExpenseManager.psm1  -verbose      # TODO PRIORITY
-Import-Module .\Modules\FleetDatabase\FleetDatabase.psm1
-Import-Module .\Modules\HomeData\HomeData.psm1
-Import-Module .\Modules\IncomeManager\IncomeManager.psm1 -verbose
-Import-Module .\Modules\InputControl\InputControl.psm1 -verbose
-Import-Module .\Modules\JsonAdapter\JsonAdapter.psm1 -verbose
-Import-Module .\Modules\StorageManager\StorageManager.psm1
-Import-Module .\Modules\StyleModule\StyleModule.psm1     -verbose         # To use style: "no style $(f "with style")"
-Import-Module .\Modules\TaskManager\TaskManager.psm1      -verbose        # TO DO           med
+Import-Module .\Modules\Cryptography\Cryptography.psm1           -verbose # Adequate
+Import-Module .\Modules\EventManager\EventManager.psm1           -verbose # TODO           high
+Import-Module .\Modules\ExpenseManager\ExpenseManager.psm1       -verbose # TODO PRIORITY
+Import-Module .\Modules\FleetDatabase\FleetDatabase.psm1                  # Adequate: report hyundai
+Import-Module .\Modules\HomeData\HomeData.psm1                            # Empty
+Import-Module .\Modules\IncomeManager\IncomeManager.psm1         -verbose # Empty?
+Import-Module .\Modules\InputControl\InputControl.psm1           -verbose #
+Import-Module .\Modules\JsonAdapter\JsonAdapter.psm1             -verbose #
+Import-Module .\Modules\StorageManager\StorageManager.psm1                #
+Import-Module .\Modules\StyleModule\StyleModule.psm1             -verbose # Functional; to use style: "no style $(f "with style")"
+Import-Module .\Modules\TaskManager\TaskManager.psm1             -verbose # TO DO           med
 Import-Module .\Modules\UserInputProcessor\UserInputProcessor.psm1 -verbose
 log 'Imported modules'
 
@@ -38,7 +38,7 @@ $DOMAIN = fromjson .\config.json ; log 'Loaded config'
 . .\Scripts\import-expenses    # expenses
 . .\Scripts\import-chores      # house
 . .\Scripts\import-events      # work schedule, regular transits, birthdays, document expiration
-# . .\Scripts\import-tasks       # one-time tasks and errands
+# . .\Scripts\import-tasks     # one-time tasks and errands
 log 'Imported .\Scripts'
 
 # Set UI settings
